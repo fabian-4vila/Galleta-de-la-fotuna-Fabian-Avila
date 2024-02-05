bf75f60fb1b02bc973d633e4e1b503895fe82715
fabian-4vila <116823446+fabian-4vila@users.noreply.github.com>
2024-02-04 19:36:27 -0500

    Update getRandomElemArray.js
    changeCode

--- src/utils/getRandomElemArray.js
+++ src/utils/getRandomElemArray.js
@@ -1,23 +1,11 @@
 const getRandomElemArray = (arr) => {
-    if (typeof getRandomElemArray.prevIndex === 'undefined') {
-        getRandomElemArray.prevIndex = -1;
-    }
-    
-    let newIndex;
-    // Un límite para evitar bucle infinito
-    const maxAttempts = arr.length * 2; 
-    
-    for (let i = 0; i < maxAttempts; i++) {
-        newIndex = Math.floor(Math.random() * arr.length);
-        if (newIndex !== getRandomElemArray.prevIndex) {
-            getRandomElemArray.prevIndex = newIndex;
-            return arr[newIndex];
-        }
-    }
-    // Actualizar prevIndex incluso si no encontramos un índice único
-    getRandomElemArray.prevIndex = newIndex; 
-    return arr[newIndex];
-}
+    let lastIndex = -1
+    let indexRandom;
+    do {
+        indexRandom = Math.floor(Math.random() * arr.length);
+    } while (indexRandom === lastIndex);
 
-export default getRandomElemArray
-
+    lastIndex = indexRandom;
+    return arr[indexRandom];
+};
+export default getRandomElemArray;
